21043c91666b896c5fd4984bf09e8125c8d5b758
Alejazzz <alejandrofutboleromolon@gmail.com>
2025-10-16 03:53:19 -0400

    Authentication controller token generation

---
+++ src/main/java/com/web/AccountService/dao/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.web.AccountService.dao;
+
+public class LoginRequest {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/web/AccountService/web/AuthController.java
+++ src/main/java/com/web/AccountService/web/AuthController.java
@@ -1,11 +1,16 @@
 package com.web.AccountService.web;
 
+import com.web.AccountService.dao.LoginRequest;
 import com.web.AccountService.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @RestController
 public class AuthController {
@@ -14,8 +19,13 @@ public class AuthController {
     TokenService tokenService;
 
     @PostMapping("/token")
-    public String token(Authentication authentication) {
-        return tokenService.generateToken(authentication);
+    public String token(@RequestBody LoginRequest loginRequest) {
+        //we need to verify that this user exists in BBDD
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(), null,
+                Collections.emptyList());
+        return tokenService.generateToken();
     }
 
     @GetMapping("/")
