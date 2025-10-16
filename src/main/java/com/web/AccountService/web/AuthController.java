7188a6fc79332cdbfb18f3e9675b4548bf6022e7
Alejazzz <alejandrofutboleromolon@gmail.com>
2025-10-16 05:20:09 -0400

    Communication between Auth and DS

--- src/main/java/com/web/AccountService/web/AuthController.java
+++ src/main/java/com/web/AccountService/web/AuthController.java
@@ -2,13 +2,17 @@ package com.web.AccountService.web;
 
 import com.web.AccountService.dao.LoginRequest;
 import com.web.AccountService.security.TokenService;
+import jdk.javadoc.doclet.Reporter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
@@ -18,14 +22,30 @@ public class AuthController {
     @Autowired
     TokenService tokenService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Value("${resource.url}")
+    String url;
+
     @PostMapping("/token")
     public String token(@RequestBody LoginRequest loginRequest) {
-        //we need to verify that this user exists in BBDD
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-                loginRequest.getUsername(), null,
-                Collections.emptyList());
-        return tokenService.generateToken();
+        String token = tokenService.generateToken();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+
+        HttpEntity<LoginRequest> entity = new HttpEntity<>(loginRequest, headers);
+
+        ResponseEntity<Boolean> response = restTemplate.postForEntity(
+                url,entity, Boolean.class);
+
+        if(Boolean.FALSE.equals(response.getBody()))
+            return "Invalid username or password";
+
+        return token;
     }
 
     @GetMapping("/")
